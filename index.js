7f75981db92c855e62f62947790132416c001a2c
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-06-24 12:17:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var height = "5'9ft";
 var country = "Nigeria";
 
 document.write("My name is ", my_name);
+console.log("My name is ", my_name);
 
 document.write("<br> My height ",height);
+console.log("My height ",height);
 
 document.write("<br> I am from ", country);
+console.log("I am from ", country)
